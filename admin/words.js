4377b4dee54e82854fd2563ad41cc644f2f28dcb
root <root@osboxes.org>
2019-12-27 20:28:19 -0500

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"go-echarger adapter settings": {
+		"en": "Adapter settings for go-echarger",
+		"de": "Adaptereinstellungen für go-echarger",
+		"ru": "Настройки адаптера для go-echarger",
+		"pt": "Configurações do adaptador para go-echarger",
+		"nl": "Adapterinstellingen voor go-echarger",
+		"fr": "Paramètres d'adaptateur pour go-echarger",
+		"it": "Impostazioni dell'adattatore per go-echarger",
+		"es": "Ajustes del adaptador para go-echarger",
+		"pl": "Ustawienia adaptera dla go-echarger",
+		"zh-cn": "go-echarger的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
